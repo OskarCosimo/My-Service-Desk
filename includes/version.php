ed547696612d7f23d70b7962c786180ba2811fa0
Oscar Cosimo <OskarCosimo@users.noreply.github.com>
2026-09-21 12:54:24 +0200

    Update application version to 1.0.40

--- includes/version.php
+++ includes/version.php
@@ -2,5 +2,5 @@
 // includes/version.php
 // Current installed application version and GitHub repository details
 
-define('APP_VERSION', '1.0.39');
+define('APP_VERSION', '1.0.40');
 define('GITHUB_REPO', 'OskarCosimo/My-Service-Desk');
